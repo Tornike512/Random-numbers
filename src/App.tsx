e90c4526de5376a81a5c5a500310d9a12f1e96ec
Tornike Tsagareishvili <dht5152@gmail.com>
2024-10-04 23:06:30 +0400

    Add: Generate Button Design

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <main className="main-container">
+      <button className="generate">Generate</button>
+      <table>
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+        </tr>
+        <tr>
+          <td>Data 1</td>
+          <td>Data 2</td>
+        </tr>
+      </table>
+    </main>
+  );
+}
+
+export default App;
